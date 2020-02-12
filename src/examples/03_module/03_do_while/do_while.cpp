8366f03556c1ad69aed36039a91f041bcf9569c9
dshampine <douglas.shampine@g.austincc.edu>
2020-02-11 18:57:50 -0600

    ex3 do while loops

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,7 +1,25 @@
 #include "do_while.h"
+#include "while.h"
+#include<iostream>
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+	int choice;
+	
+	do
+	{
+		int num;
+		std::cout << "Enter a number: ";
+		std::cin >> num;
+			std::cout << sum_of_squares(num) << "\n\n";
+		std::cout << "Continue: ";
+		std::cin >> choice;
+	} while (choice == 1);
+}
+
+
 
 //Write code for function run_menu that prompts  user for a 
 //number from 1 to 4 and displays the option user selected.
@@ -10,4 +28,5 @@ Use the existing menu_option function from /example/02_module/03_switch
 folder.
 
 */
+
 
