4ef049d5f4c3421af1fe27fd6e8bffb03ea9b515
dshampine <douglas.shampine@g.austincc.edu>
2020-02-16 12:10:52 -0600

    homework 3 files

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -6,6 +6,21 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string & dna)
+{
+	double total =0;
+
+	for (int i = 0; i < dna.length(); ++i)
+	{
+		if (dna[i] == 'C' || dna[i] == 'G')
+		{
+			total += 1;
+		}
+	}
+
+	return total / dna.length();
+}
+
 
 
 
@@ -14,6 +29,17 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna)
+{
+	string result;
+
+	for (int i = dna.length()-1; i >= 0; --i)
+	{
+		result += dna[i];
+	}
+	return result;
+}
+
 
 
 
@@ -27,4 +53,32 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna)
+{
+	string dna_reverse;
+	dna_reverse = get_reverse_string(dna);
+	string result;
+
+	for (int i = 0; i < dna_reverse.length(); ++i)
+	{
+		if (dna_reverse[i] == 'A')
+		{
+			result += 'T';
+		}
+		else if (dna_reverse[i] == 'T')
+		{
+			result += 'A';
+		}
+		else if (dna_reverse[i] == 'G')
+		{
+			result += 'C';
+		}
+		else if (dna_reverse[i] == 'C')
+		{
+			result += 'G';
+		}
+	}
+
+	return result;
+}
 
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include<iostream>
+#include "dna.h"
 //write using statements
-
+using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,31 @@ user enters a y or Y.
 */
 int main() 
 {
+	int menu_prompt = 0; string dna_input; string choice;
+
+	do
+	{
+		cout << "Press 1 to get GC-content for a DNA string.\n";
+		cout << "Press 2 to get reverse complement for DNA string.\n";
+		cin >> menu_prompt;
+		cout << "Enter DNA string: ";
+		cin >> dna_input;
+
+		if (menu_prompt == 1)
+		{
+			cout << "GC-content for DNA string " << dna_input << " : " << get_gc_content(dna_input);
+		}
+		else if (menu_prompt == 2)
+		{
+			cout << "Reverse complement for DNA string " << dna_input << " : " << get_dna_complement(dna_input);
+		}
+
+		cout << "\n\n** Press Y or y to run again or any other key to quit. ** ";
+		cin >> choice;
+
+
+	} while (choice == "Y" || choice == "y");
+	
+
 	return 0;
 }
