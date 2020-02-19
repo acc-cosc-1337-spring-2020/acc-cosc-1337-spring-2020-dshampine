e019eff9c96155f72f98d44d315d7f7fdc3bfd09
dshampine <douglas.shampine@g.austincc.edu>
2020-02-18 18:29:54 -0600

    for ranged loops and reference values

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -44,3 +44,19 @@ void loop_string_w_auto(const std::string & str)
 		std::cout << ch << "\n";
 	}
 }
+
+void loop_string_w_auto_value(std::string &str)
+{
+	for (auto ch : str)
+	{
+		ch = 'z';
+	}
+}
+
+void loop_string_w_auto_ref(std::string &str)
+{
+	for (auto &ch : str)
+	{
+		ch = 'z';
+	}
+}
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -2,6 +2,8 @@
 #include "catch.hpp"
 #include "while.h"
 #include "value_ref.h"
+#include "for_ranged.h"
+#include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -22,4 +24,22 @@ TEST_CASE("Test pass by val and ref function")
 
 	REQUIRE(num1 == 5);
 	REQUIRE(num2 == 50);
+}
+
+TEST_CASE("Test for ranged loop by value")
+{
+	std::string name = "Joe";
+
+	loop_string_w_auto_value(name);
+
+	REQUIRE(name == "Joe");
+}
+
+TEST_CASE("Test for ranged loop by ref")
+{
+	std::string name = "Joe";
+
+	loop_string_w_auto_ref(name);
+
+	REQUIRE(name == "zzz");
 }
