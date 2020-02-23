52aaed153ce0b1ece614ac19b691105ceb54a658
Douglas Shampine <douglas.shampine@g.austincc.edu>
2020-02-23 17:16:11 -0600

    added vector of primes function and unit test.

--- src/homework/04_vectors/vectors.cpp
+++ src/homework/04_vectors/vectors.cpp
@@ -1,3 +1,5 @@
+#include"vectors.h"
+using std::vector;
 /*
 Write a value return function get_max_from_vector with a const reference 
 vector of intsparameter that returns the max value in a vector
@@ -5,7 +7,10 @@ vector of intsparameter that returns the max value in a vector
 @param numbers is a const reference to a vector of integers
 @return the max value in the vector
 */
-
+int get_max_from_vector(const vector<int>& vec)
+{
+	return 0;
+}
 
 
 /*
@@ -15,6 +20,19 @@ given a number returns true if prime or false if not prime
 @param number: Any whole number
 @return: bool if prime False if not
 */
+bool is_prime(int num)
+{
+	if (num <= 1) {
+		return false;
+	}
+	for (int i = 2; i < num; ++i) {
+		if (num % i == 0) {
+			return false;
+		}
+		
+	}
+	return true;
+}
 
 /*
 Write a a function named vector_of_primes with an integer parameter
@@ -28,3 +46,15 @@ Example given number 10 returns a vector with elements 2,3,5,7,
 Make sure to use the is_prime function to determine if current 
 number is prime.
 */
+vector<int> vector_of_primes(int value)
+{
+	vector<int> primes{};
+
+	for (int i = 2; i <= value; ++i) {
+		if (is_prime(i)) {
+			primes.push_back(i);
+		}
+	}
+
+	return primes;
+}
--- test/homework_test/04_vectors_test/04_vectors_tests.cpp
+++ test/homework_test/04_vectors_test/04_vectors_tests.cpp
@@ -1,7 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "vectors.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test prime numbers function")
+{
+	REQUIRE(is_prime(-1) == false);
+	REQUIRE(is_prime(1) == false);
+	REQUIRE(is_prime(2) == true);
+	REQUIRE(is_prime(3) == true);
+	REQUIRE(is_prime(4) == false);
+	REQUIRE(is_prime(5) == true);
+	REQUIRE(is_prime(9) == false);
+	REQUIRE(is_prime(43) == true);
+	REQUIRE(is_prime(44) == false);
+}
+
+TEST_CASE("Test get primes funtion")
+{
+	std::vector<int> expected1{ 2, 3, 5, 7};
+	std::vector<int> expected2{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	REQUIRE(vector_of_primes(10) == expected1);
+	REQUIRE(vector_of_primes(50) == expected2);
 }
 
