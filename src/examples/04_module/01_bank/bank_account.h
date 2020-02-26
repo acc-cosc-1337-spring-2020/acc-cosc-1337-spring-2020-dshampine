41edadf0fa28cae2991ba3d5535585f90fd7dc23
dshampine <douglas.shampine@g.austincc.edu>
2020-02-25 19:07:30 -0600

    module 4 OPP examples

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,1 +1,11 @@
 //bank_account.h
+class BankAccount
+{
+public:
+	BankAccount(int b) : balance{ b } {}
+	int get_balance()const { return balance; } // const makes it so function can't be changed e.g. balance can't be changed before return
+	void deposit(int amount);
+	void withdraw(int amount);
+private:
+	int balance; //balance can be changed, not a const.
+};
--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,23 @@
-
+#include "bank_account.h"
+#include <iostream>
+using std::cout;
 int main()
 {
+	/*auto num{ 1 };
+	auto num2{ 1.5 };
+	auto c{ 'c' };
+	auto flag{ true }; */
 
+
+
+	BankAccount account(500);
+	auto balance = account.get_balance();
+	
+	cout << "Balance is: " << balance;
+
+	auto amount{ 0 };
+	cout << "Enter deposit amount: ";
+	std::cin >> amount;
+	
 	return 0;
 }
