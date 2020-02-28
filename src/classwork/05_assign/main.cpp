88348bcd812eb4c1c511ecc31380c271f7a1902c
dshampine <douglas.shampine@g.austincc.edu>
2020-02-27 19:22:31 -0600

    classword 05

--- src/classwork/05_assign/main.cpp
+++ src/classwork/05_assign/main.cpp
@@ -1,5 +1,8 @@
 //write include statemetns
-
+#include "rectangle.h"
+#include <iostream>
+#include <vector>
+using std::cin; using std::cout;
 /*
 Create a vector of rectangles
 Add 3 Rectangle classes to the vector:
@@ -12,6 +15,13 @@ Iterate the vector and display the Area for each Rectangle on one line and the t
 */
 int main()
 {
+	Rect area();
+	std::vector<Rect> area{ Rect(4, 5), area(10, 10), area(100, 10) };
+
+		for (auto vec : area)
+		{
+			cout << vec;
+		}
 	
 	return 0;
 }
--- src/classwork/05_assign/rectangle.h
+++ src/classwork/05_assign/rectangle.h
@@ -3,3 +3,14 @@ Create the interface for a Rectangle class with one constructor that accepts two
 has one public function area that returns the area of the rectangle, and one private function calculate_area
 that calculates area of the rectangle.  The class has 3 int private variables area, width, and height.
 */
+class Rect
+{
+public:
+	Rect(int h, int w) : height{ h }, width{ w } {};
+	
+private:
+	void calculate_area(int w, int h);
+	int area;
+	int width;
+	int height;
+};
