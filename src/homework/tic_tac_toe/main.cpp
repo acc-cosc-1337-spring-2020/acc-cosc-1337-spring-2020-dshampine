5f8decee3abfd1dfe0f2698d11eb48994eca72b0
dshampine <douglas.shampine@g.austincc.edu>
2020-03-04 21:29:35 -0600

    added game startup loop in main.cpp

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,25 @@
+#include <iostream>
+#include "tic_tac_toe.h"
+#include "string"
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	int board_position;
+	string choice;
+	
+	TicTacToe player_one;
+	player_one.start_game("X");
+
+	do 
+	{
+		cout << "Enter board position 1 - 9: ";
+		cin >> board_position;
+		player_one.mark_board(board_position);
+		cout << "\nContinue Y or N ";
+		cin >> choice;
+
+	} while (choice == "Y" || choice == "y");
 
 	return 0;
 }
