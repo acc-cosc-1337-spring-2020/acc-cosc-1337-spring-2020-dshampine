23f28816710a68b6ef8fea4aae53e5754d25fd75
dshampine <douglas.shampine@g.austincc.edu>
2020-04-06 21:28:34 -0500

    updated manger cpp

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -8,6 +8,15 @@ void TicTacToeManager::save_game(const TicTacToe b)
 
 void TicTacToeManager::update_winner_count(string winner)
 {
+	if (winner == "X")
+	{
+		x_win += 1;
+	}
+	else if (winner == "O")
+	{
+		o_win += 1;
+	}
+	else ties += 1;
 }
 
 std::ostream & operator<<(std::ostream & out, const TicTacToeManager & manager)
