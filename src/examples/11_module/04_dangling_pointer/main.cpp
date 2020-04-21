10c6abbbe401e9f876263168b3e78967e181bd01
dshampine <douglas.shampine@g.austincc.edu>
2020-04-21 18:06:42 -0500

    dangling pointer

--- src/examples/11_module/04_dangling_pointer/main.cpp
+++ src/examples/11_module/04_dangling_pointer/main.cpp
@@ -1,8 +1,16 @@
 #include "dangling_pointer.h"
-
+#include <iostream>
 int main() 
 {
-	
+	int* ptr_num = new int(5);
+	int* ptr = ptr_num;
+
+	std::cout << *ptr; //need to dereference * to cout
+
+	delete ptr_num;
+	ptr_num = nullptr;
+
+	std::cout << *ptr;
 
 	return 0;
 }
