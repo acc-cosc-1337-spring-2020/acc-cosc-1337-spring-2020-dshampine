0565b3cdb69716a77f67f42875869139dbad3f97
dshampine <douglas.shampine@g.austincc.edu>
2020-04-21 19:14:31 -0500

    class copy heap memory management

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,2 +1,23 @@
 //
 //TODO 7 add template
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+#include<cstddef>
+#include<stddef.h>
+
+class Vector 
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v); //copy constructor
+	size_t Size()const { return size; }
+	//void set_size(size_t sz) { size = sz; }
+	int& operator[](int i) { return nums[i]; } //allows modification of array
+	int& operator[](int i)const { return nums[i]; }
+private:
+	size_t size;
+	int* nums; 
+};
+
+#endif // !MY_VECTOR_H
+
