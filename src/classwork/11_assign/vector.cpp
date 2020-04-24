d4462660110693dc6cf788b48ffcce1ee65dbe15
dshampine <douglas.shampine@g.austincc.edu>
2020-04-23 19:05:20 -0500

    vector copy assignment

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -27,6 +27,32 @@ Vector::Vector(const Vector & v)
 	}
 
 }
+/*
+C++ 98 method
+Allocate temporay dynamic array of size v(v1)
+Copy v1 elements to temp array
+Deallocate old v2 nums array
+Point v2 nums array to temp array
+Set v2 size to v1 size
+return self copy of Vector
+*/
+
+Vector & Vector::operator=(const Vector & v)
+{
+	int* temp = new int[v.size];
+
+	for (size_t i = 0; i < v.size; ++i)
+	{
+		temp[i] = v[i];
+	}
+
+	delete nums;
+	nums = temp;
+	size = v.size;
+
+	return *this;
+	// TODO: insert return statement here
+}
 
 /*
 Release dynammic memory
--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -4,17 +4,20 @@
 #define MY_VECTOR_H
 #include<cstddef>
 #include<stddef.h>
-
+/*
+Rule of 3 C++98 for memory management 
+*/
 class Vector 
 {
 public:
 	Vector(size_t sz);
-	Vector(const Vector& v); //copy constructor
+	Vector(const Vector& v); //copy constructor - Rule of 3
+	Vector& operator=(const Vector& v);//copy assignment - Rule of 3
 	size_t Size()const { return size; }
 	//void set_size(size_t sz) { size = sz; }
 	int& operator[](int i) { return nums[i]; } //allows modification of array
 	int& operator[](int i)const { return nums[i]; }
-	~Vector();//destructor
+	~Vector();//destructor - Rule of 3
 private:
 	size_t size;
 	int* nums; 
